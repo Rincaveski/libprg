e742d427e84ea9710562b5ce2a65c9a06fd24521
rincaveski <jonatasrincaveski@gmail.com>
2025-09-29 08:17:50 -0300

    Lab 5 finalizado

--- libprg/src/libprg/no.c
+++ libprg/src/libprg/no.c
@@ -1,27 +1,27 @@
-#include <stdlib.h>
-
-#include "libprg/libprg.h"
-
-typedef struct no {
-    int valor;
-    no_t* proximo;
-} no_t;
-
-no_t* criar_no(int valor) {
-    no_t* no = malloc(sizeof(no_t));
-    no->valor = valor;
-    no->proximo = NULL;
-
-    return no;
-}
-
-no_t adicionar_no(int valor, no_t** inicio) {
-    no_t* no = criar_no(valor);
-    no->proximo = &inicio;
-    inicio = no
-
-}
-
-// buscar
-// remover
-// destruir
+// #include <stdlib.h>
+//
+// #include "libprg/libprg.h"
+//
+// typedef struct no {
+//     int valor;
+//     no_t* proximo;
+// } no_t;
+//
+// no_t* criar_no(int valor) {
+//     no_t* no = malloc(sizeof(no_t));
+//     no->valor = valor;
+//     no->proximo = NULL;
+//
+//     return no;
+// }
+//
+// no_t adicionar_no(int valor, no_t** inicio) {
+//     no_t* no = criar_no(valor);
+//     no->proximo = &inicio;
+//     inicio = no
+//
+// }
+//
+// // buscar
+// // remover
+// // destruir
